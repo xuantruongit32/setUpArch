4a1c002160f7a9863cee4d6a6202e2320ea28a2a
Hoang Truong <88026544+xuantruongit32@users.noreply.github.com>
2023-11-18 19:41:31 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm neovim firefox zsh i3 terminator yay flameshot xpdf curl wget code pavucontrol unrar unzip 7z
+
+##touch-cursor
+git clone https://github.com/donniebreve/touchcursor-linux
+cd touchcursor-linux
+make
+sudo make install
+##Config 
+sudo systemctl --user restart touchcursor.service
+
+
+##OhMyZsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+
